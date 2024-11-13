281f5bcfb6b28d716fbdb91e1fbf1afe6095170c
Tichy958 <106491842+Tichy958@users.noreply.github.com>
2024-11-13 18:09:10 +0100

    final projectttt

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 document.querySelector(".fa-solid").addEventListener("click", function () {
   document.querySelector(".outer-grid").classList.toggle("active");
 });
 document.querySelector(".fa-solid").addEventListener("click", function () {
   document.querySelector(".hide-nav").classList.toggle("active");
 });
-=======
-function testFunction() {
-  window.alert("is working");
-}
->>>>>>> 24fb7c8 (finished project css holy grail)
